bfa9797798f5c11a529702c16d71608248c177ae
Tyler <tmcgrego47@gmail.com>
2021-12-10 20:22:02 -0700

    added comments to writeExcelFile

--- src/main/java/WriteExcelFile.java
+++ src/main/java/WriteExcelFile.java
@@ -29,16 +29,20 @@ public class WriteExcelFile {
             Cell cell = headerRow.createCell(i);
             cell.setCellValue(Constants.columns[i]);
             cell.setCellStyle(headerCellStyle);
-        }
+        } //gets correct position for row number for entries
         int rowNum = 1;
         for(int i = 0; i < Constants.NUM_IMG_TO_GEN; i++) {
             Row row = sheet.createRow(rowNum++);
             row.setHeight((short) 1000);
-            Object key = oddsMap.keySet().toArray()[i];
-            Object value = oddsMap.values().toArray()[i];
+            Object key = oddsMap.keySet().toArray()[i]; //gets image name index
+            Object value = oddsMap.values().toArray()[i]; //gets rarity for that image
+            //adds image to accurately describe image name and creates an entry for it
             row.createCell(0).setCellValue("img" + key);
-            row.createCell(1).setCellValue(String.valueOf(value));
+            //trims rarity % and adds an entry for it
+            row.createCell(1).setCellValue(String.valueOf(value).substring(0,6));
             try {
+                //reads files that were previously written to, gets that image as a byte[] array, then writes that image
+                //that is bound between the anchor points
                 InputStream inputStream = new FileInputStream(Constants.outputFiles[i]);
                 byte[] bytes = IOUtils.toByteArray(inputStream);
                 int pictureId = workbook.addPicture(bytes, Workbook.PICTURE_TYPE_PNG);
@@ -49,28 +53,22 @@ public class WriteExcelFile {
                 anchor.setRow1(rowNum-1);
                 anchor.setCol2(3);
                 anchor.setRow2(rowNum);
-                XSSFPicture my_picture = drawing.createPicture(anchor, pictureId);
+                drawing.createPicture(anchor, pictureId);
                 // Resize all columns to fit the content size
 
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+        } //autoSizes columns in case of bad spacing
         for (int j = 0; j < Constants.columns.length; j++) {
             sheet.autoSizeColumn(j);
-        }
-        FileOutputStream fileOut = null;
+        } //Writing of the spreadsheet
+        FileOutputStream fileOut;
         try {
             fileOut = new FileOutputStream("Rarity_SpreadSheet.xlsx");
             workbook.write(fileOut);
             fileOut.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NullPointerException e) {
+        } catch (IOException | NullPointerException e) {
             e.printStackTrace();
         }
     }
